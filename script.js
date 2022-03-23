3053c43e5fa9015c6448553733b71a16414f5a58
TR742 <Tyrich1442@gmail.com>
2022-03-22 19:43:01 -0700

    initian text removed upon button click

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
+// Selectors
+let main = document.querySelector("#main");
+
 // Event Listeners
 startBtn.addEventListener("click", startQuiz);
+
+function startQuiz () {
+    let question = document.getElementById("main");
+    question.remove();
+};
